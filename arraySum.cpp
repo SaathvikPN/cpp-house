bb20cae7c4c0c41f5ba0d5d0562d276b446d4df9
Saathvik PN <66002135+SaathvikPN@users.noreply.github.com>
2020-09-02 10:05:00 +0530

    Create arraySum.cpp

---
+++ arraySum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a[2], b[2];
+    for(int i=0; i<n; i++)
+    {
+        cin>>array[i];
+        sum=sum + array[i];
+    }
+    cout<<sum;
+    return 0;
+}
